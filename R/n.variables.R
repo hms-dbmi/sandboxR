97f6186c9bca7f1c99191572a151ae973c600307
gversmee <34459351+gversmee@users.noreply.github.com>
2018-10-11 13:07:48 -0400

    no message

--- R/n.variables.R
+++ R/n.variables.R
@@ -13,6 +13,7 @@
 
 n.variables <- function(phs)  {
 
+  phs <- phs.version(phs)
   url<- paste0("ftp://anonymous:anonymous@ftp.ncbi.nlm.nih.gov/dbgap/studies/", unlist(strsplit(phs, "\\."))[1], "/", phs, "/")
 
   filenames <- strsplit(RCurl::getURL(url, ftp.use.epsv = TRUE, dirlistonly = TRUE), "\n")[[1]]
